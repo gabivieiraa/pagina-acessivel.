095626e6339438f56483d0fe5a0c1bb2a930a50a
gabivieiraa <159184847+gabivieiraa@users.noreply.github.com>
2025-03-18 11:18:35 -0300

    18-03

---
+++ script.js
@@ -0,0 +1,5 @@
+document .addEventListener('DOMContentLoaded, function'()){
+    const AumentaFonteBotão = document. getElementById('aumentar-font')
+    const DiminuiFonteBotão = document. getElementById('diminuir-font')
+}
+let tamanhoatualDafonte =1
