a39f307ec1930fe653d1945580b0d89f1fcb9b6e
gabivieiraa <159184847+gabivieiraa@users.noreply.github.com>
2025-04-02 11:14:04 -0300

    02-04-2025

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded',function(){
 
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    botaoAcessibilidade.addEventListener('click',function());
+    
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     let tamanhoAtualFonte= 1;
